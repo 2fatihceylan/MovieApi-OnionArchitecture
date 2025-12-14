497c143c9e4e2e31dea4d57dc24c67ae86ac823c
2fatihceylan <49781433+2fatihceylan@users.noreply.github.com>
2025-12-14 13:57:16 +0300

    UI app created

---
+++ Frontends/MovieApi.WebUI/ViewComponents/UserLayoutWebUIViewComponents/_UserLayoutWebUILoginModalComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.ViewComponents.UserLayoutWebUIViewComponents
+{
+    public class _UserLayoutWebUILoginModalComponentPartial : ViewComponent
+    {
+
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/MovieApi.WebUI/ViewComponents/UserLayoutWebUIViewComponents/_UserLayoutWebUINavBarComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.ViewComponents.UserLayoutWebUIViewComponents
+{
+    public class _UserLayoutWebUINavBarComponentPartial : ViewComponent
+    {
+
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
