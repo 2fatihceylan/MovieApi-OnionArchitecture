57918eea9fbf6dbcb5876aeb94d7b718be0d4cc6
2fatihceylan <49781433+2fatihceylan@users.noreply.github.com>
2025-12-30 18:02:21 +0300

    adminMovieListResult

---
+++ Frontends/MovieApi.Dto/Dtos/AdminMovieDtos/AdminResultMovieDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieApi.Dto.Dtos.AdminMovieDtos
+{
+    public class AdminResultMovieDto
+    {
+
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string CoverImageUrl { get; set; }
+
+        public decimal? Rating { get; set; }
+        public string Description { get; set; }
+        public string Duration { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string CreatedYear { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Frontends/MovieApi.WebUI/Areas/Admin/Controllers/AdminMovieController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Dto.Dtos.AdminMovieDtos;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace MovieApi.WebUI.Areas.Admin.Controllers
+{
+
+
+
+   
+    [Area("Admin")]
+    public class AdminMovieController : Controller
+    {
+
+
+
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminMovieController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+
+
+
+
+
+
+
+        public async Task<IActionResult> MovieList()
+        {
+
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage = await client.GetAsync("https://localhost:7216/api/Movie");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<AdminResultMovieDto>>(jsonData);
+
+                return View(values);
+            }
+
+
+
+            return View();
+        }
+    }
+}
