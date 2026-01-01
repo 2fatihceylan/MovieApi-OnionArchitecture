78e379135c6d78692dddae21d5287dca12283de5
2fatihceylan <49781433+2fatihceylan@users.noreply.github.com>
2026-01-01 13:38:25 +0300

    admin category list, admin create category

---
+++ Frontends/MovieApi.Dto/Dtos/AdminCategoryDtos/AdminCreateCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieApi.Dto.Dtos.AdminCategoryDtos
+{
+    public class AdminCreateCategoryDto
+    {
+      
+        public string CategoryName { get; set; }
+
+     
+    }
+}
---
+++ Frontends/MovieApi.Dto/Dtos/AdminCategoryDtos/AdminResultCategoryDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieApi.Dto.Dtos.AdminCategoryDtos
+{
+    public class AdminResultCategoryDto
+    {
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public int MovieCount { get; set; }
+        public int ReviewCount { get; set; }
+        public double AvgRating { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
